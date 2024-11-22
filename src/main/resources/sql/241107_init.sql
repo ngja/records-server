5ef7e0bd73e7f66961d057aa6175a86c7d3dab28
hansol <wkdqhrh00@naver.com>
2024-11-22 23:58:13 +0900

    update ddl

--- src/main/resources/sql/241107_init.sql
+++ src/main/resources/sql/241107_init.sql
@@ -23,3 +23,103 @@ CREATE TABLE person
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE artist
+(
+    id           BIGINT       NOT NULL,
+    name         VARCHAR(255) NOT NULL,
+    description  VARCHAR(255) NOT NULL,
+    debut_date   TIMESTAMP    NOT NULL,
+    status       VARCHAR(255) NOT NULL,
+    image_width  INTEGER      NOT NULL,
+    image_height INTEGER      NOT NULL,
+    image_path   VARCHAR(255),
+    deleted      BOOLEAN   DEFAULT FALSE,
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pk_artist PRIMARY KEY (id)
+);
+
+CREATE TABLE member
+(
+    id           BIGINT       NOT NULL,
+    artist_id    BIGINT       NOT NULL,
+    name         VARCHAR(255) NOT NULL,
+    image_width  INTEGER      NOT NULL,
+    image_height INTEGER      NOT NULL,
+    image_path   VARCHAR(255),
+    deleted      BOOLEAN      NOT NULL,
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pk_member PRIMARY KEY (id)
+);
+
+CREATE TABLE album
+(
+    id           BIGINT       NOT NULL,
+    artist_id    BIGINT       NOT NULL,
+    name         VARCHAR(255) NOT NULL,
+    release_date TIMESTAMP    NOT NULL,
+    tags         VARCHAR(255),
+    image_width  INTEGER      NOT NULL,
+    image_height INTEGER      NOT NULL,
+    image_path   VARCHAR(255),
+    deleted      BOOLEAN      NOT NULL,
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pk_album PRIMARY KEY (id)
+);
+
+CREATE TABLE mv
+(
+    id         BIGINT       NOT NULL,
+    artist_id  BIGINT,
+    song_id    BIGINT,
+    url        VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted    BOOLEAN      NOT NULL,
+    CONSTRAINT pk_mv PRIMARY KEY (id)
+);
+
+CREATE TABLE concert
+(
+    id           BIGINT       NOT NULL,
+    name         VARCHAR(255) NOT NULL,
+    type         VARCHAR(255) NOT NULL,
+    start_date   TIMESTAMP    NOT NULL,
+    end_date     TIMESTAMP    NOT NULL,
+    image_width  INTEGER      NOT NULL,
+    image_height INTEGER      NOT NULL,
+    image_path   VARCHAR(255),
+    deleted      BOOLEAN      NOT NULL,
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pk_concert PRIMARY KEY (id)
+);
+
+CREATE TABLE concert_artist
+(
+    id         BIGINT  NOT NULL,
+    artist_id  BIGINT,
+    concert_id BIGINT,
+    deleted    BOOLEAN NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pk_concert_artist PRIMARY KEY (id)
+);
+
+CREATE TABLE song
+(
+    id           BIGINT       NOT NULL,
+    album_id     BIGINT,
+    title        VARCHAR(255) NOT NULL,
+    lyrics       VARCHAR(255) NOT NULL,
+    composition  VARCHAR(255) NOT NULL,
+    arrangement  VARCHAR(255) NOT NULL,
+    release_date TIMESTAMP    NOT NULL,
+    deleted      BOOLEAN      NOT NULL,
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT pk_song PRIMARY KEY (id)
+);
